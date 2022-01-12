122e94238f2e30ee2dd1ad330e4344b33917554d
abijoy <mdalaminbijoy3@gmail.com>
2022-01-12 18:47:34 +0600

    fixed some deprecated stuff

--- friends_fire/playground/urls.py
+++ friends_fire/playground/urls.py
@@ -6,8 +6,8 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
 	path('signup/', views.SignUp, name='signup'),
-	path('accounts/login/', auth_views.login, {'template_name': 'playground/login.html'}, name='login'),
-	path('accounts/logout/', auth_views.logout, {'template_name': 'playground/logout.html'}, name='logout'),
+	path('accounts/login/', auth_views.LoginView.as_view(), {'template_name': 'playground/login.html'}, name='login'),
+	path('accounts/logout/', auth_views.LogoutView.as_view(), {'template_name': 'playground/logout.html'}, name='logout'),
 	path('', views.home, name='home'),
 	path('post/<int:post_id>/', views.post_details, name='post_details'),
 	path('post/upvote/<int:post_id>/', views.upvote, name='upvote'),
